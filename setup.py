09f2cded6643c8cc32385bf4d8ba7173cb3ed169
Will Roberts <will.roberts@rz.hu-berlin.de>
2015-12-14 20:35:15 +0100

    setup.py pylint cleanups

--- setup.py
+++ setup.py
@@ -14,14 +14,14 @@ from setuptools import setup, find_packages
 from codecs import open
 from os import path
 
-here = path.abspath(path.dirname(__file__))
+HERE = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
-    long_description = f.read()
+with open(path.join(HERE, 'README.rst'), encoding='utf-8') as f:
+    LONG_DESCRIPTION = f.read()
 
-with open(path.join(here, 'fsed', 'VERSION'), encoding='utf-8') as f:
-    version = f.read().strip()
+with open(path.join(HERE, 'fsed', 'VERSION'), encoding='utf-8') as f:
+    VERSION = f.read().strip()
 
 setup(
     name='fsed',
@@ -29,10 +29,10 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version=version,
+    version=VERSION,
 
     description='Aho-Corasick string replacement utility',
-    long_description=long_description,
+    long_description=LONG_DESCRIPTION,
 
     # The project's main homepage.
     url='https://github.com/wroberts/fsed',
