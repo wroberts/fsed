f4fc9734122af4ed1ba1268ea364acbf3ac29b30
Will Roberts <will.roberts@rz.hu-berlin.de>
2015-12-14 18:02:29 +0100

    fsed/tests/test_ahocorasick.py: whitespace

--- fsed/tests/test_ahocorasick.py
+++ fsed/tests/test_ahocorasick.py
@@ -8,7 +8,6 @@ test_ahocorasick.py
 Unit tests for the ``ahocorasick`` module.
 '''
 
-
 from __future__ import absolute_import, print_function, unicode_literals
 from .. import ahocorasick
 import unittest
